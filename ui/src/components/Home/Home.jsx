57519d83576adb22c56ace4f4ab9a47f6b6334b0
shivdeepsingh124 <157642009+shivdeepsingh124@users.noreply.github.com>
2024-02-09 16:40:39 -0500

    Product Listing uload page

--- ui/src/components/Home/Home.jsx
+++ ui/src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signup from '../Signup/Signup.jsx';
 import Login from '../Login/Login.jsx';
 import Product_Listing from '../Product_Listing/Product_Listing.jsx';
+import ListingUpload from '../listing_upload/ListingUpload.jsx';
 
 const Home = () => {
     return (
@@ -15,6 +16,7 @@ const Home = () => {
                     <Route path='signup' element={<Signup />} />
                     <Route path='login' element={<Login />} />
                     <Route path='productListings' element={<Product_Listing/>} />
+                    <Route path='ListingsUpload' element={<ListingUpload/>} />
                 </Routes>
             </BrowserRouter>
             <Footer />
