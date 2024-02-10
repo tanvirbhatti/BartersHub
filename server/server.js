42f92f98d98ab11e32707d8c8d955cd2951a41a5
Rahul <40555494+Rahulki@users.noreply.github.com>
2024-02-09 22:46:32 -0500

    removed older controller files and moved new one in Controller folder

--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { registerUser } from './userController.js';
-import { login } from './userController.js';
+import { registerUser } from './Controllers/registerController.js';
+import { login } from './Controllers/loginController.js';
+import {getProducts} from './Controllers/getProductController.js'
 import cors from 'cors';
+import session from 'express-session';
 
 const app = express();
 app.use(cors({ origin: 'http://localhost:3000', credentials: true, methods: ["GET", 'POST', 'PUT', 'DELETE'], },))
 app.use(bodyParser.json());
+app.use(session({
+  secret:"It's top secret",
+  resave:false,
+  saveUninitialized:false
+}))
+
 // API End-points
 app.post('/register', registerUser);
 app.post('/login', login)
+app.get('/products',getProducts)
 
 // Start the server
 const PORT = process.env.PORT || 8000;
