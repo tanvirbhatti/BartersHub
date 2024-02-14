a5f523757ca020f033a92828ae2cc821bfc7ba33
VMGhelani <157642044+VMGhelani@users.noreply.github.com>
2024-02-14 23:03:16 +0530

    Add Changes

--- server/Controllers/getProductController.js
+++ server/Controllers/getProductController.js
@@ -1,20 +1,28 @@
-import jwt from 'jsonwebtoken';
-
-const secretKey = "abcd"
 
 export async function getProducts(req,res){
-    const token = req.session.token
-    if(!token){
-        res.json({err:"Unauthorized access no token"})
-    }
-    else{
-        try { 
-            // Verify JWT
-            const decodedToken = jwt.verify(token, secretKey);
-            // User is authenticated
-            res.json({ message: `welcome ${decodedToken.firstName}` });
-        } catch (error) {
-            return res.status(401).json({ err: 'Unauthorized access' });
+    try {
+  
+        const db = await connectToDb();
+    
+        const { userId } = req.body;
+    
+        if (!userId) {
+          return res.json({ error: "userId are required." });
         }
-    }
+    
+        // Find user by email
+        const result = await db.collection('product').find({ userId });
+    
+        if (!result) {
+          return res.json({ error: "product not found." });
+        }
+        else{
+            return res.json({ message: "Product ", product: result });
+        }
+    
+        
+      } catch (error) {
+        console.error("Error during get product:", error);
+        return res.json({ error: "Internal server error." });
+      }
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import { registerUser } from './Controllers/registerController.js';
 import { login } from './Controllers/loginController.js';
 import {getProducts} from './Controllers/getProductController.js'
+import { addProduct } from './Controllers/addProductController.js'
 import cors from 'cors';
 import session from 'express-session';
 
@@ -18,7 +19,8 @@ app.use(session({
 // API End-points
 app.post('/register', registerUser);
 app.post('/login', login)
-app.get('/products',getProducts)
+app.get('/products',getProducts);
+app.post('/addProduct', addProduct);
 
 // Start the server
 const PORT = process.env.PORT || 8000;
