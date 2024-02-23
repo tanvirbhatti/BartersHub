190208214f33d9b3519402039a720cf7866e3081
Rahul <40555494+Rahulki@users.noreply.github.com>
2024-02-23 17:44:33 -0500

    add controllers for testimonials

---
+++ server/Controllers/Testimonials/add.js
@@ -0,0 +1,60 @@
+import { connectToDb } from '../../db.js';
+import  jwt from 'jsonwebtoken';
+
+
+export async function addTestimonial(req, res) {
+    try {
+
+        const db = await connectToDb();
+        const secretKey = 'abcd'; 
+        
+        let userId;
+        // Verify and decode the token
+        const token = req.session.token;
+        if(!token){
+            res.status(401).json({error:"login error please login before you add the product"})
+        }
+        else{
+            jwt.verify(token, secretKey, (err, decoded) => {
+                if (err) {
+                    // Handle verification error
+                    res.status(401).json('Token verification failed:', err);
+                } else {
+                    // Extract userId from decoded token
+                    userId = decoded.userId;
+                }
+            });
+            
+            if(!userId){
+                res.status(401.01).json({error:"token is not trusted try login again"});
+            }
+            else{
+                
+                const { testimonialText, rating, userConsent, testimonialProduct} = req.body;
+    
+                if (!testimonialText) {
+                    return res.json({ error: "Testimonial text is reqiured" });
+                }
+                //Required field validation
+                if (!testimonialText || !userConsent) {
+                    return res.json({ error: "Please provide all required fields" });
+                }
+
+                const timeStamp = new Date();
+                const newTestimonial = await db.collection('testimonials').insertOne(
+                    { 
+                        userId, testimonialText, rating, userConsent, testimonialProduct, timeStamp
+                    });
+                return res.status(200).json({ message: 'Product added successfully', newTestimonial });
+    
+            }
+        }
+    } catch (error) {
+        console.error("Error during add testimonial:", error);
+        return res.json({ error: "Internal server error." });
+    }
+}
+
+
+
+
---
+++ server/Controllers/Testimonials/get.js
@@ -0,0 +1,19 @@
+import  {connectToDb}  from "../../db.js";
+
+export async function getTestimonials(req, res) {
+  const db = await connectToDb();
+
+  try {
+    const cursor = db.collection("testimonials").find().limit(3);
+    const foundTestimonials = await cursor.toArray();
+
+    if (foundTestimonials.length <= 0) {
+      return res.json({ error: "couldn't find any testimonials" });
+    }
+    return res.status(200).json({"list of testimonials": foundTestimonials});
+  
+  } catch (err) {
+    console.log(err)
+    return res.json({"error get testimonials":err});
+  }
+}
