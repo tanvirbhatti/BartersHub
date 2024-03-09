95d7251dd2a8dfcb2f67ceb615f552c47ca798f0
Rahul <40555494+Rahulki@users.noreply.github.com>
2024-03-09 01:44:56 -0500

    modify Product_Homepage/index.js for integration with back-end-services

--- ui/src/components/Product_HomePage/index.js
+++ ui/src/components/Product_HomePage/index.js
@@ -2,87 +2,27 @@ import React,{useEffect, useState} from 'react'
 import "./Product_Home.css"
 import Testimonials from './Testimonials';
 
-
-const _items = [
-    {
-        id: 1,
-        imageUrl: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-        price: '$1800.00',
-        description: '2 BHK house for rent',
-        location: 'waterloo'
-    },
-    {
-        id: 2,
-        imageUrl: 'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg',
-        price: '$1800.00',
-        description: '2 BHK house for rent',
-        location: 'waterloo'
-    },
-    {
-        id: 3,
-        imageUrl: 'https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTExL3BmLXMxMDgtcG0tNDExMy1tb2NrdXAuanBn.jpg',
-        price: '$1800.00',
-        description: '2 BHK house for rent',
-        location: 'waterloo'
-    },
-    {
-        id: 4,
-        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIYJ0Y6KCRkAXnRZ5XnOcGI2C92ggGWMqClpyVkm2fN4hcQU6Ex7VCZPWMs_22MSXA5HQ&usqp=CAU',
-        price: '$1800.00',
-        description: '2 BHK house for rent',
-        location: 'waterloo'
-    },
-    {
-        id: 5,
-        imageUrl: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-        price: '$1800.00',
-        description: '2 BHK house for rent',
-        location: 'waterloo'
-    },
-    {
-        id: 6,
-        imageUrl: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-        price: '$1800.00',
-        description: '2 BHK house for rent',
-        location: 'waterloo'
-    },
-    {
-        id: 7,
-        imageUrl: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-        price: '$1800.00',
-        description: '2 BHK house for rent',
-        location: 'waterloo'
-    },
-    {
-        id: 8,
-        imageUrl: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-        price: '$1800.00',
-        description: '2 BHK house for rent',
-        location: 'waterloo'
-    },
-    {
-        id: 9,
-        imageUrl: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-        price: '$69999.00',
-        description: '4 BHK house for rent',
-        location: 'waterloo'
-    }
-];
-
 const ProductHome = () => {
     const [products,setProducts] = useState([])
+    const [recentlyListedProducts, setRecentlyListedProducts] = useState([])
+
     useEffect(() => {
         // Fetch data from the API
-        
         fetch("http://localhost:8000/get-featured-products")
-            .then(response => response.json())
-            .then(data => {
-                // Assuming your response data structure is { "list of products": [...] }
-                setProducts(data);
-                console.log(products.featuredProducts)
-            })
-            .catch(error => console.error('Error fetching data:', error));
+        .then(response => response.json())
+        .then(data => {
+            setProducts(data);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+
+        fetch("http://localhost:8000/get-recently-products")
+        .then(response => response.json())
+        .then(data =>{
+            setRecentlyListedProducts(data);
+        })
+        .catch(error=>console.error('Error fetching data:',error));
     }, []);
+
     return (
         <>
             <div className="row p-5 align-items-center no_gutter">
@@ -157,9 +97,12 @@ const ProductHome = () => {
                 <div className="product-list-container p-5">
                     <h3><b>Recently Listed:</b></h3>
                     <div className="scrolling-wrapper">
-                        {_items.map((product) => (
-                            <div key={product.id} className="product-card border-0 shadow-none">
-                                <img src={product.imageUrl} alt={product.title} style={{ width: '100%', height: '200px' }} />
+                        {
+                            recentlyListedProducts && recentlyListedProducts.map
+                            (
+                                (product,index)=>
+                                <div key={index} className="product-card border-0 shadow-none">
+                                <img src={product.image} alt={product.title} style={{ width: '100%', height: '200px' }} />
                                 <div className="card-body">
                                     <h5 className="card-title">{product.title}</h5>
                                     <p className="card-text">{product.description}</p>
@@ -167,8 +110,8 @@ const ProductHome = () => {
                                     <button className='btn btn-sm view_button'>View Product</button>
                                 </div>
                             </div>
-                        ))}
-                       
+                            )
+                        }
                     </div>
                     <h3 className='pt-5 pb-3'><b>Testimonials:</b></h3>
                     {
