d1a2b69a7e34246287f28ce3c3928101f625d57c
Rahul <40555494+Rahulki@users.noreply.github.com>
2024-03-25 13:59:39 -0400

    modify Product.jsx to post the product in featured collection

--- ui/src/components/AdminPanel/Products.jsx
+++ ui/src/components/AdminPanel/Products.jsx
@@ -1,20 +1,46 @@
 import React, { useEffect, useState } from "react";
 import ToggleButton from "../../UI/ToggleButton/ToggleButton";
+import {toast} from 'react-toastify'
 
 export const Products = () => {
     const [products,setProducts] = useState([])
+    const [productId, setProductId] = useState({})
+    const [isToggled,setIsToggled] = useState(false)
 
     useEffect(()=>{
         fetch("http://localhost:8000/get-products")
         .then(res=>res.json())
-        .then(data=>setProducts(data["list of products"]))
+        .then(data=>{
+            setProducts(data["list of products"])})
         .catch(err=>console.error("error fetching data", err))
     },[]);
     
-    const handleToggleChange = (index)=>{
-        console.log("toggle changed",index)
-    } 
+    const handleToggleChange = (toggleState,product_id)=>{
+        setIsToggled(toggleState)
+        setProductId(product_id)
+    }
     
+    useEffect(()=>{
+        if(isToggled){
+            if(window.confirm("Are you sure you want to add it in featured collection?")){
+                fetch("http://localhost:8000/add-featured-product",
+                {
+                    method:"POST",
+                    headers:{
+                        "Content-Type":"application/json"
+                    },
+                    body:JSON.stringify({productId:productId})
+                })
+                .then(res=>res.json())
+                .then(data=>toast.success(data.message))
+                .catch(err=>console.error("error sending fetch request",err))
+            }
+            else{
+                setIsToggled(false);
+            }
+        }
+    },[isToggled,productId])
+
     return (
             <table className="w-100 p-3"> 
                 <thead className="bg-dark text-white align-items-center text-center">
@@ -36,7 +62,7 @@ export const Products = () => {
                                     <img src={product.image} alt={product.title} style={{width : '100%', height:'70px'}} />
                                 </td>
                                 <td className="col-md-2">{product.title}</td>
-                                <td className="col-md-3 justify-content-center d-flex">Featured Product <ToggleButton onClick = {()=>handleToggleChange(index)}/></td>
+                                <td className="col-md-3 justify-content-center d-flex">Featured Product <ToggleButton onToggle={(toggleState)=>handleToggleChange(toggleState,product._id)} /></td>
                                 <td className="col-md-3">{product.email}</td>
                                 <td className="col-md-1">city</td>
                                 <td className="col-md-1">
