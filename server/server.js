3f1ec61b534a93f74228113dbbb9f59e734f17fc
Rahul <40555494+Rahulki@users.noreply.github.com>
2024-03-27 15:08:42 -0400

    arrange all the endpoints

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ import { addFeaturedProduct } from './Controllers/FeaturedProducts/add.js';
 import { getFeaturedProducts } from './Controllers/FeaturedProducts/get.js';
 import { getRecentlyListedProducts } from './Controllers/RecentlyAddedProducts/get.js';
 import { deleteFeaturedProduct } from './Controllers/FeaturedProducts/delete.js';
+import { deleteProduct } from './Controllers/Products/delete.js';
 
 
 const app = express();
@@ -45,6 +46,8 @@ app.get('/get-product/:id', getProductById)
 //Home page endpoints
 app.get('/get-testimonials', getTestimonials)
 app.post('/add-testimonial', addTestimonial)
+app.get('/get-featured-products', getFeaturedProducts)
+app.get('/get-recently-products', getRecentlyListedProducts);
 
 //User data endpoints
 app.get('/userprofile/:id', userProfile)
@@ -52,26 +55,14 @@ app.get('/user-listings', checkUser, getUserListings)
 app.post('/edit-product', checkUser, updateListing);
 app.delete('/delete-product/:id', checkUser, deleteListing);
 
-app.post('/add-featured-product', addFeaturedProduct)
-app.get('/get-featured-products', getFeaturedProducts)
-app.get('/get-recently-products', getRecentlyListedProducts);
-app.delete('/delete-featured-product', deleteFeaturedProduct);
-
-app.put('/users/:userId/disable', checkUser, disableUser); // Endpoint to disable a user
-
-app.put('/users/:userId/enable', checkUser, enableUser);//Endpoint to enable a user
-
-app.delete('/users/delete/:userId', checkUser, deleteUser); // Endpoint to delete a user
-
-// Route to fetch all users
-app.get('/users', async (req, res) => {
-  try {
-    await fetchAllUsers(req, res);
-  } catch (error) {
-    console.error('Error fetching all users:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+//Admin Panel endpoints
+app.post('/admin/add-featured-product', addFeaturedProduct)
+app.delete('/admin/delete-featured-product', deleteFeaturedProduct);
+app.delete('/admin/delete-product/:productId',checkUser,deleteProduct)
+app.put('/admin/disable-user/:userId', checkUser, disableUser);
+app.put('/admin/enable-user/:userId', checkUser, enableUser);
+app.delete('/admin/delete-user/:userId', checkUser, deleteUser);
+app.get('/admin/users', fetchAllUsers);
 
 
 // Start the server
