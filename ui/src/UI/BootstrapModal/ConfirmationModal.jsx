9308a9cda808c7c2b0a3249089324d2b4516c09c
Rahul <40555494+Rahulki@users.noreply.github.com>
2024-04-03 22:10:46 -0400

    Add Bootstrap Modal component to replace Confirm Dialogbox

---
+++ ui/src/UI/BootstrapModal/ConfirmationModal.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ConfirmationModal = ({ isOpen, onClose, onConfirm, message }) => {
+  return (
+    <div className={`modal fade ${isOpen ? 'show' : ''}`} style={{ display: isOpen ? 'block' : 'none' }} tabIndex="-1" role="dialog">
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title col-md-11">Confirmation</h5>
+            <button type="button" className="close btn btn-danger col-md-1" data-dismiss="modal" aria-label="Close" onClick={onClose}>
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <p>{message}</p>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" onClick={onClose}>Cancel</button>
+            <button type="button" className="btn btn-primary" onClick={onConfirm}>Confirm</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmationModal;
