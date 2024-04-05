d5294ca948755037f95a72b826531fc36ccfb5ce
Rahul <40555494+Rahulki@users.noreply.github.com>
2024-04-05 03:37:21 -0400

    changed Navbar to modify the navigation options for different user types(admin and/or user)

--- ui/src/UI/Nav/Nav.jsx
+++ ui/src/UI/Nav/Nav.jsx
@@ -2,45 +2,115 @@ import React, { useEffect, useState } from "react";
 import GradientButton from "../GradientButton/GradientButton";
 import Logo from "./BarterHub.png";
 import styles from "./Nav.module.css";
+import { jwtDecode } from "jwt-decode";
+import { toast } from "react-toastify";
+import ConfirmationModal from '../../UI/BootstrapModal/ConfirmationModal';
 
 const Nav = () => {
   const [user, setUser] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [confirmAction, setConfirmAction] = useState(null);
+  const [confirmMessage, setConfirmMessage] = useState("");
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
-      setUser({ token }); 
+      // Decode token and set user data
+      const decoded = jwtDecode(token);
+      setUser({
+        ...decoded,
+        token // Store the token if needed for further requests
+      });
     }
   }, []);
 
-  
-
+  const handleLogout = async () => {
+    setConfirmMessage("Are you sure you want to logout?");
+    setConfirmAction(() => () => {
+      fetch('http://localhost:8000/logout', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${user.token}`
+        },
+      })
+        .then(response => {
+          if (response) {
+            setUser(null);
+            localStorage.removeItem('token');
+            toast.success(response.message);
+            setModalOpen(false)
+          } else {
+            toast.error('Logout failed!');
+            setModalOpen(false)
+          }
+        })
+        .catch(error => { console.error("Error during logout:", error) });
+      setModalOpen(false)
+    })
+    setModalOpen(true)
+  };
   return (
-    <nav className={styles.Nav}>
-      <img className={styles.Logo} src={Logo} alt="logo of barter hub" />
-      <div className={styles["search-container"]}>
-        <input
-          className={styles.search}
-          type="text"
-          placeholder="Type to search"
-        ></input>
-      </div>
-      <ul className={styles.items}>
-        <li><a href="/">Home</a></li>
-        <li><a href="/productListings">Listing</a></li>
-        <li>
-          {user ? (
-            <a href="/user">
-              <GradientButton rounded={false} text="User Profile" />
-            </a>
-          ) : (
-            <a href="/login">
-              <GradientButton rounded={false} text="Login" />
-            </a>
-          )}
-        </li>
-      </ul>
-    </nav>
+    <>
+      <ConfirmationModal
+        isOpen={modalOpen}
+        onClose={() => setModalOpen(false)}
+        onConfirm={confirmAction}
+        message={confirmMessage}
+        />
+      <nav className={styles.Nav}>
+        <img className={styles.Logo} src={Logo} alt="logo of barter hub" />
+        <div className={styles["search-container"]}>
+          <input
+            className={styles.searchBox}
+            type="text"
+            placeholder="Type to search"
+          ></input>
+        </div>
+        <ul className={styles.items}>
+          <li><a href="/">Home</a></li>
+          <li><a href="/productListings">Listing</a></li>
+          <li>{/* Default: Render login */}
+            {user && (
+              <>
+                {user.userType === "admin" ? (
+                  <>
+                    {/* Render admin-specific components */}
+                    <div className="d-flex gap-2">
+                      <a href="/admin">
+                        <GradientButton rounded={true} text="Admin Panel" />
+                      </a>
+                      <button style={{ border: 0, background: 'white' }} onClick={handleLogout}>
+                        <i class={`fa fa-sign-out border ${styles.logout}`}></i>
+                      </button>
+                    </div>
+                  </>
+                ) : user.userType === "user" ? (
+                  <>
+                    {/* Render user-specific components */}
+                    <a href="/user">
+                      <GradientButton rounded={true} text="User Profile" />
+                    </a>
+                    <button style={{ border: 0, background: 'white' }} onClick={handleLogout}>
+                        <i class={`fa fa-sign-out border ${styles.logout}`}></i>
+                    </button>
+                  </>
+                ) : (
+                  <a href="/login">
+                    <GradientButton rounded={true} text="Login" />
+                  </a>
+                )}
+              </>
+            )}
+            {!user && ( /* Render login if user is not defined */
+              <a href="/login">
+                <GradientButton rounded={true} text="Login" />
+              </a>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
