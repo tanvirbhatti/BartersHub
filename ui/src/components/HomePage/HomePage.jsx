9bb2bfa6fe4b0894353d18de893094cc2cffff98
Rahul <40555494+Rahulki@users.noreply.github.com>
2024-04-12 17:58:16 -0400

    add sprite sheet for Images and remove old photos apply the required css for the same

--- ui/src/components/HomePage/HomePage.jsx
+++ ui/src/components/HomePage/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import "../../Assets/Stylesheets/Components/Product_Home.css"
 import Testimonials from './Testimonials';
-import Hero from "../../Assets/Images/heroImage.png";
 import FeaturedProducts from './FeaturedProducts';
 import RecentlyListedProducts from './RecentlyListedProducts';
 import Categories from './Categories';
@@ -18,16 +17,17 @@ const ProductHome = () => {
     return (
         <>
             <div className="row p-5 align-items-center no_gutter">
-                <div className="col-md-7">
-                    <h4 className='text-dark'><b>Empower your exchanges, cultivate connections, thrive with Barters Hub today!</b></h4>
+                <div className="col-md-7 col-lg-8">
+                    <h4 className='HeroText text-dark fw-bold'>Empower your exchanges, cultivate connections, thrive with Barters Hub today!</h4>
                     <div className='pt-3'>
                         <div className="col-md-3">
                             <GradientButton text="Add Your Product" rounded={true} onClick={handleAddProduct}/>
                         </div>
-                    </div>
+                    </div>  
                 </div>
-                <div className="col-md-5 text-center">
-                    <img src={Hero} className="img-fluid w-100" alt="" />
+                <div className="col-md-5 col-lg-4 text-center">
+                    {/* <img src={Hero} className="img-fluid w-100" alt="" /> */}
+                    <div className='HeroImage img-fluid'></div>
                 </div>
             </div>
 
