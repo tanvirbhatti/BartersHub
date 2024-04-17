7a5198ac1355d96d012118508db123ea59e3ede8
Rahul <40555494+Rahulki@users.noreply.github.com>
2024-04-16 20:54:58 -0400

    add new component for default url path

---
+++ ui/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const NotFound = () =>{
+    return(
+        <div className="p-5 m-5 text-center">
+                <div className="p-5 m-3">
+                    <h1 className="text-danger fw-bold p-5 m-5">404 : Page Not Found</h1>
+                </div>
+        </div>
+    )
+}
