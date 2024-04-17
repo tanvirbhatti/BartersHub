ed2e63630b085d686c0306937fbdba8324b612f1
Parth1704 <parth.patel704@gmail.com>
2024-04-16 23:57:08 -0400

    updated css, added favicon and updated listing page with required field mark

--- ui/src/components/listing_upload/ListingUpload.jsx
+++ ui/src/components/listing_upload/ListingUpload.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useNavigate } from 'react-router-dom'; 
 import { toast } from 'react-toastify';
 import GradientButton from '../../UI/GradientButton/GradientButton';
+
 const ListingUpload = () => {
     const [formData, setFormData] = useState({
         title: '',
@@ -146,25 +147,35 @@ const ListingUpload = () => {
             )}
 
             <h3 className='text-center p-2 mb-3'>Post Product Ad</h3>
+            <p className="required-text">Fields marked with <span className="required-indicator">*</span> are required</p>
             <form className="product-ad-form rounded px-4 pt-2 pb-4 shadow " onSubmit={handleSubmit}>
 
-                <label htmlFor="title">Product Title</label>
+                <label htmlFor="title">Product Title<span className="required-indicator">*</span></label>
                 <input className="form-control rounded border-dark" type="text" id="title" name="title" value={formData.title} onChange={handleInputChange} />
 
                 {errors.title && <div className="error-message">{errors.title}</div>}
 
-                <label htmlFor="description">Product Description</label>
+                <label htmlFor="description">Product Description<span className="required-indicator">*</span></label>
                 <textarea className="form-control rounded border-dark" id="description" name="description" value={formData.description} onChange={handleInputChange}></textarea>
                 {errors.description && <div className="error-message">{errors.description}</div>}
 
                 <label htmlFor="category">Select a Category</label>
                 <select id="category" name="category" value={formData.category} onChange={handleInputChange} className='rounded form-select border-dark'>
-                    <option value="category1">Category 1</option>
-                    <option value="category2">Category 2</option>
+                    <option value="category1">Electronics</option>
+                    <option value="category2">Furniture</option>
+                    <option value="category3">Fashion</option>
+                    <option value="category4">Toys & Games</option>
+                    <option value="category5">Property Rentals</option>
+                    <option value="category6">Home</option>
+                    <option value="category7">Pet Supplies</option>
+                    <option value="category8">Others</option>
+
+
                 </select>
 
                 <div className="form-group rounded border border-dark p-3">
                     <label>Add photos of your product <span className="required-indicator">*</span></label>
+                    <small className="form-text text-muted">Upload JPG, JPEG, or PNG only</small>
                     <hr />
                     <input className='form-control-file' type="file" accept="image/jpeg, image/png" onChange={handleImageSelect} />
                     {errors.image && <div className="error-message" style={{marginTop:"10px"}}>{errors.image}</div>}
@@ -176,7 +187,7 @@ const ListingUpload = () => {
                 </div>
 
 
-                <label htmlFor="price">Price</label>
+                <label htmlFor="price">Price<span className="required-indicator">*</span></label>
                 <input className="form-control rounded border-dark" type="number" id="price" name="price" value={formData.price} onChange={handleInputChange} />
                 {errors.price && <span className="error">{errors.price}</span>}
 
@@ -184,13 +195,13 @@ const ListingUpload = () => {
                     <p>Contact Information</p>
                     <hr />
                     <div className='contact-info-input'>
-                        <label htmlFor="phoneNumber">Phone Number:</label>
-                        <input type="tel" id="phoneNumber" className='form-control rounded' name="phoneNumber" value={formData.phoneNumber} onChange={handleInputChange} />
+                        <label htmlFor="phoneNumber">Phone Number:<span className="required-indicator">*</span></label>
+                        <input type="tel" id="phoneNumber" className='form-control rounded border-dark' name="phoneNumber" value={formData.phoneNumber} onChange={handleInputChange} />
                         {errors.phoneNumber && <span className="error">{errors.phoneNumber}</span>}
                     </div>
                     <div className='contact-info-input'>
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" className='form-control rounded' id="email" name="email" value={formData.email} onChange={handleInputChange} />
+                        <label htmlFor="email">Email:<span className="required-indicator">*</span></label>
+                        <input type="email" className='form-control rounded border-dark' id="email" name="email" value={formData.email} onChange={handleInputChange} />
                         {errors.email && <span className="error">{errors.email}</span>}
                     </div>
                 </fieldset>
